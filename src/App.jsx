4017935104bfec4d5c70244c91b8e67127f5a14f
sametorbuk <orbuk60@gmail.com>
2024-07-05 11:31:31 +0300

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import axios from "axios"
 import './App.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas, far, fab } from '@fortawesome/free-solid-svg-icons'
+library.add(fas, far, fab);
+import PleasedOrNotButton from './components/unpleased-button'
+
+
 
 function App() {
  
 
   return (
     <>
-      
+      <PleasedOrNotButton></PleasedOrNotButton>
     </>
   )
 }
---
+++ src/components/unpleased-button.jsx
@@ -0,0 +1,18 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./unpleased-button.css"
+
+
+export default function PleasedOrNotButton() {
+
+return (
+   <>
+   <Button className="unpleased-button" ><FontAwesomeIcon icon={faFaceFrown}/>İlgimi çekmiyor</Button>
+   
+   <Button className="unpleased-button"><FontAwesomeIcon icon={faFaceFrown}/>Bu gündem başlığı zararlı veya spam içeriyor</Button>
+   
+   </>
+
+)
+
+
+}
